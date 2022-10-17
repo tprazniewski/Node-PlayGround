61edfa76f02f9961157bbb32165035199bd82f2d
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-17 18:08:32 +0200

    Create root directory util

--- Express/app.js
+++ Express/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const path = require('path')
+const rootDir = require('./utils/path')
 // const http = require('http')
 
 const app = express();
@@ -23,7 +24,7 @@ app.use(('/admin'), adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'))
 })
 
 
--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 
+const rootDir = require('../utils/path')
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-        return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+        return res.sendFile(path.join(rootDir,'views', 'add-product.html'))
+        // return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
     // return res.send(` <form action='/admin/product' method='POST'> <input type='text' name='title'> <button type='submit'> Send</button>  </form>`)
 })
 
