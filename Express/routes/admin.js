cc462f02a6772cadb14be7b2017f78e63cd57601
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-18 19:06:20 +0200

    Create ejs template engine

--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const rootDir = require('../utils/path')
+const rootDir = require("../utils/path");
 const router = express.Router();
 
 const products = [];
-router.get('/add-product', (req, res, next) => {
-        // return res.sendFile(path.join(rootDir,'views', 'add-product.html'))
-        // return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
-    // return res.send(` <form action='/admin/product' method='POST'> <input type='text' name='title'> <button type='submit'> Send</button>  </form>`)
+router.get("/add-product", (req, res, next) => {
+  // return res.sendFile(path.join(rootDir,'views', 'add-product.html'))
+  // return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  // return res.send(` <form action='/admin/product' method='POST'> <input type='text' name='title'> <button type='submit'> Send</button>  </form>`)
 
-    res.render('add-product',{ pageTitle: "Add product!", active: 'admin', formsCSS: true, productCSS: true, activeProduct: true})
+  res.render("add-product", {
+    pageTitle: "Add product!",
+    active: "admin",
+    formsCSS: true,
+    productCSS: true,
+    activeProduct: true,
+  });
+});
 
-})
+router.post("/add-product", (req, res, next) => {
+  const { title } = req.body;
+  console.log({ title });
+  products.push({ title });
+  res.redirect("/");
 
-router.post('/add-product', (req, res, next) => {
-    const {title} = req.body
-    console.log({title})
-    products.push({title})
-    res.redirect('/')
-
-
-    // next()
-})
+  // next()
+});
 
 // module.exports = router
 exports.adminRoutes = router;
