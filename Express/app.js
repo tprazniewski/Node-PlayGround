0122f657ba05bfbda833b0dfcba7add93782fe35
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-18 14:59:37 +0200

    Add dynamic title

--- Express/app.js
+++ Express/app.js
@@ -32,7 +32,7 @@ app.use(shopRoutes)
 app.use((req,res,next)=>{
     // res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'))
 
-    res.status(404).render('404')
+    res.status(404).render('404', {pageTitle: 'Page not Found!!' })
 })
 
 
