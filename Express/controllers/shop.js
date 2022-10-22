ba92ad173f5a8e7f0a9050062f15ce32ccbc530c
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-22 22:59:46 +0200

    Refactor getProducts functionality

--- Express/controllers/shop.js
+++ Express/controllers/shop.js
@@ -3,42 +3,15 @@ const {Cart} = require('../models/cart')
 
 
 const getProducts = async (req, res, next) => {
-  // res.send('<h1> Hello from Shope Routes </h1>')
-  // res.sendFile(path.join(__dirname,'..',  'views', 'shop.html'))
-
-  // --------------------------------------------------------------------------------------------
-  // console.log('from shop',products)
-  // return res.sendFile(path.join(rootDir,'views', 'shop.html'))
-  // --------------------------------------------------------------------------------------------
-  //V1
-
-  // const products = await Product.findAll()
-  // const products2 = JSON.parse(products)
-
-  // console.log(products2.length)
-  // console.log('controllers>products> getProducts:',products2)
-  // res.render("shop", {
-  //   productList: products2,
-  //   pageTitle: "Shopping!",
-  //   active: "shop",
-  //   isProduct: products2.length > 0,
-  //   activeShop: true,
-  //   productCSS: true
-  // });
-
-  // --------------------------------------------------------------------------------------------
-  //V2
-  Product.findAll((products) => {
+  Product.findAll()
+  .then(([rows,data]) =>{
     res.render("shop/product-list", {
-      productList: products,
-      pageTitle: "Shopping!",
-      active: "shop",
-      //for handlepars views engine
-      // isProduct: products.length > 0,
-      // activeShop: true,
-      // productCSS: true,
-    });
-  });
+        productList: rows,
+        pageTitle: "Shopping!",
+        active: "shop"
+      });
+  })
+  .catch(err=> console.log(err))
 };
 
 const getindex = (req, res, next) => {
